ca138ee80fee40bacd2c26365cb96a38942c0148
Yordis Colina <yordi.colina@untoquedigital.com>
2025-01-18 23:27:53 -0500

    remove warnings and TODO

--- lib/infraestructure/errors/api_error_handler.dart
+++ lib/infraestructure/errors/api_error_handler.dart
@@ -2,20 +2,18 @@ import 'package:dartz/dartz.dart';
 import 'dart:io';
 
 class ApiErrorHandler {
-  /// Maneja errores y retorna un mensaje descriptivo como un `Either`.
   static Either<String, T> handle<T>(dynamic error) {
     if (error is SocketException) {
-      return Left('Network error: No internet connection.');
+      return const Left('Network error: No internet connection.');
     } else if (error is HttpException) {
-      return Left('HTTP error: Failed to fetch data from the server.');
+      return const Left('HTTP error: Failed to fetch data from the server.');
     } else if (error is FormatException) {
-      return Left('Data format error: Invalid response format.');
+      return const Left('Data format error: Invalid response format.');
     } else {
       return Left('Unexpected error: ${error.toString()}');
     }
   }
 
-  /// Evalúa una función asíncrona y encapsula el resultado en un `Either`.
   static Future<Either<String, T>> execute<T>(Future<T> Function() action) async {
     try {
       final result = await action();
